da37f14be9ac45eef5e5e52f6f61638bd5aceb77
benrobson <benrobson76@gmail.com>
2021-02-23 19:27:25 +1100

    Commit project init files.

---
+++ src/main/java/net/craftingforchrist/zanderinstance/ZanderInstanceMain.java
@@ -0,0 +1,35 @@
+package net.craftingforchrist.zanderinstance;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ZanderInstanceMain extends JavaPlugin {
+    public static ZanderInstanceMain plugin;
+    private Connection connection;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        // Plugin Load Message
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "\n\n" + plugin.getDescription().getName() + " is now enabled.\nRunning Version: " + plugin.getDescription().getVersion() + "\nGitHub Repository: https://github.com/craftingforchrist/zander-instance\nCreated By: " + plugin.getDescription().getAuthors() + "\n\n");
+
+        // Plugin Event Register
+        PluginManager pluginmanager = this.getServer().getPluginManager();
+//        pluginmanager.registerEvents(new EggFindEvent(), this);
+
+        plugin.saveDefaultConfig(); // Generate configuration file
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin Shutdown Message
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "\n\n" + plugin.getDescription().getName() + " is now disabled.\n\n");
+    }
+
+}
